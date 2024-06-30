0d51faa042b2ac2c03ef2af3a68b48b0320b2d8a
Anncoo <dnjsalsdn2@gmail.com>
2024-06-30 22:21:35 +0900

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://drive.google.com/drive/folders/17fTlBArpfDpBaSV6AIb2dFX-otNp7UyK?usp=sharing/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://anncoo.dothome.co.kr/Anncoo/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
